1876edd7e9698dbbd18db97377a6da1dcc4286c1
unknown <ajc8467@uncw.edu>
2020-04-02 13:41:44 -0400

    completed smtp homework...

--- SMTPExtraction/smtp_email.py
+++ SMTPExtraction/smtp_email.py
@@ -3,19 +3,25 @@ import smtplib, ssl
 # choosing the smtp port
 port = 465 
 pswrd = input("Gmail Password: ")
+dummy_file = input("What file did you want to generate an email from? ")
 
 # Create SSL context
 context = ssl.create_default_context()
 
 # sending a message directly from python (no txt file)
-sender_email = "hellotest001smtp@gmail.com"
+sender_email = "hellotest002smtp@gmail.com"
 receiver_email = "hellotest001smtp@gmail.com"
-message = """\
-    Subject: Hi There
+message = " "
 
-    This message was sent to you via Python."""
+# opens up the txt file and fills the message variable with info from the file
+with open(dummy_file) as line:
+    for x in line:
+        message += x
+
+print(message)      # check that the message is being pulled from the dummy txt file
 
 # makes sure that the connection is severed when complete
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login("hellotest001smtp@gmail.com", pswrd)
+    server.login(sender_email, pswrd)
+    print("Sending messages")
     server.sendmail(sender_email, receiver_email, message)
