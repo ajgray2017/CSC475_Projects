ce29f4309d8bb4efa874161e615f4e4dfae4aec1
Adam Gray <ajgray2017@gmail.com>
2020-04-28 13:47:29 -0400

    init

--- Botnet/UDPclient.py
+++ Botnet/UDPclient.py
@@ -1,9 +1,33 @@
 from socket import *
+import smtplib, ssl
+import getpass
+
 serverName = '127.0.0.1'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = input('Input lowercase sentence:')
+
 clientSocket.sendto(message.encode(),(serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
+lst, serverAddress = clientSocket.recvfrom(2048)
+sender_email, receiver_email, dummy_file, = lst[0], lst[1], lst[2] 
 clientSocket.close()
+
+# choosing the smtp port
+port = 465 
+pswrd = getpass.getpass()       # getpass function that will get the password w/o showing it
+
+# Create SSL context
+context = ssl.create_default_context()
+
+# opens up the txt file and fills the message variable with info from the file
+with open(dummy_file) as line:
+    for x in line:
+        message += x
+
+print(message)      # check that the message is being pulled from the dummy txt file
+
+# makes sure that the connection is severed when complete
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login(sender_email, pswrd)
+    print("Sending messages")
+    server.sendmail(sender_email, receiver_email, message)
--- Botnet/UDPserver.py
+++ Botnet/UDPserver.py
@@ -2,8 +2,18 @@ from socket import *
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
-print('The server is ready to receive')
+
+print('The server is ready to destroy your enemies and get arrested for wire fraud')
 while True:
- message, clientAddress = serverSocket.recvfrom(2048)
- modifiedMessage = message.decode().upper()
- serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+    print("waiting...")
+    message, clientAddress = serverSocket.recvfrom(2048)
+
+    print(message)
+
+    # sender_email = input("Sender Email.. ")
+    # receiver_email = input("Receiver Email.. ")
+    # dummy_file = input("File.. ")
+
+    lst = ["hellotest002smtp@gmail.com", "hellotest001smtp@gmail.com", "email_body.txt"]
+
+    serverSocket.sendto(lst.encode(), clientAddress)
