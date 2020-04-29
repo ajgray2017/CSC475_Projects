9ef1fc1ef0129683a748ec649133b98617e4b530
Adam Gray <ajgray2017@gmail.com>
2020-04-28 20:14:23 -0400

    small file

--- Botnet/UDPclient.py
+++ Botnet/UDPclient.py
@@ -1,33 +1,20 @@
 from socket import *
-import smtplib, ssl
-import getpass
+import pickle
 
 serverName = '127.0.0.1'
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input lowercase sentence:')
+client = socket(AF_INET, SOCK_DGRAM)
 
-clientSocket.sendto(message.encode(),(serverName, serverPort))
-lst, serverAddress = clientSocket.recvfrom(2048)
-sender_email, receiver_email, dummy_file, = lst[0], lst[1], lst[2] 
-clientSocket.close()
+input("ready? ")
 
-# choosing the smtp port
-port = 465 
-pswrd = getpass.getpass()       # getpass function that will get the password w/o showing it
+client.sendto("Slave Running".encode(),(serverName, serverPort))
+data, serverAddress = client.recvfrom(2048)
 
-# Create SSL context
-context = ssl.create_default_context()
+work = pickle.loads(data)
 
-# opens up the txt file and fills the message variable with info from the file
-with open(dummy_file) as line:
-    for x in line:
-        message += x
+if work.sendEmail():
+    client.sendto("Email Sent".encode(),(serverName, serverPort))
+else:
+    client.sendto("Failure".encode(),(serverName, serverPort))
 
-print(message)      # check that the message is being pulled from the dummy txt file
-
-# makes sure that the connection is severed when complete
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login(sender_email, pswrd)
-    print("Sending messages")
-    server.sendmail(sender_email, receiver_email, message)
+client.close()
